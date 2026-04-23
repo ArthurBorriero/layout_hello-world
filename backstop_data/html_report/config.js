ba9bfe3b1ed2569b5ae68cf8d7823c73dbb52598
Arthur <arthur.borriero.silva@gmail,com>
2026-04-23 20:26:14 -0300

    fixed again

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/Entire_document.png",
-        "test": "../bitmaps_test/20260423-094435/Entire_document.png",
+        "test": "../bitmaps_test/20260423-103026/Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
